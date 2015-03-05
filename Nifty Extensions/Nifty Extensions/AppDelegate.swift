873c198714688c09f633e0ee3454ed19f7a802ec
anli5005 <li.anthony917@gmail.com>
2015-03-05 10:46:48 -0500

    Initial Xcode commit

---
+++ Nifty Extensions/Nifty Extensions/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Nifty Extensions
+//
+//  Created by Anthony Li on 3/5/15.
+//  Copyright (c) 2015 anli5005. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Nifty Extensions/Nifty Extensions/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Nifty Extensions
+//
+//  Created by Anthony Li on 3/5/15.
+//  Copyright (c) 2015 anli5005. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
